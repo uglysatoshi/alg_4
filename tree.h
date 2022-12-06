ed684c38116225b7d8df8be28050c4e8c2b3a601
uglysatoshi <uglysatoshi@ya.ru>
2022-12-07 01:02:24 +0300

    v2

--- tree.h
+++ tree.h
@@ -21,31 +21,37 @@ class Tree
 {  
     private:
         TNode *root; 
-    public:
-        TNode* GetTreeRoot();
+    protected:
         TNode* RecursionSearch(TNode* node, int value);
-        TNode* CreateTreeElement(int value); 
         TNode* RecursionInsert(TNode* node, int value);
         TNode* RemoveByValueRecursion(TNode* node, int value);
         TNode* RemoveMinRecursion(TNode* node);
         void ClearTreeRecursion(TNode* node);
-        void ClearTree();
-        void ClearBranch(int value);
         int TotalLeafsRecursion(TNode* node);
-        int TotalLeafs();
         int TotalNodesRecursion(TNode* node);
-        int TotalNodes();
         int NextNodeRecursion(TNode* node);
         int PreNodeRecursion(TNode* node);
         int SearchForMaxRecursion(TNode* node);
-        int SearchForMax(TNode* node);
         int SearchForMinRecursion(TNode* node);
+        int HeightRecursion(TNode* node);
+        void DepthIteration(TNode* node);
+        void WidthIteration(TNode* node);
+        void RecursionPrint(TNode* node, int padding);
+        int SearchForEqualRecursion(TNode* node);
+        bool AnyTreeSearchRecursion(TNode* node, int value);
+    public:
+        TNode* GetTreeRoot();
+        TNode* CreateTreeElement(int value); 
+        void ClearTree();
+        void ClearBranch(int value);
+        int TotalLeafs();
+        int TotalNodes();
+        int SearchForMax(TNode* node);
         int SearchForMin(TNode* node);
         int NextNode(int value);
         int PreNode(int value);
         int Extent(int value);
         int Level(int value);
-        int HeightRecursion(TNode* node);
         int Height();
         void Insert(int value);
         void RemoveByValue(int value);
@@ -53,14 +59,9 @@ class Tree
         void RemoveMin();
         void Print();
         void Depth();
-        void DepthIteration(TNode* node);
         void Width();
-        void WidthIteration(TNode* node);
-        void RecursionPrint(TNode* node, int padding);
-        int SearchForEqualRecursion(TNode* node);
         int SearchForEqual();
         bool Search(int value);
-        bool AnyTreeSearchRecursion(TNode* node, int value);
         bool AnyTreeSearch(int value);
         Tree();
         ~Tree();
